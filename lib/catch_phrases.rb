1b92aaa61620642eeb226508b7f3adb7ebb4693a
Barak Rosner <rosnerbb@gmail.com>
2021-02-02 23:34:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,19 +1,20 @@
 def mario
   $phrase1 = "It's-a me, Mario!"
-  puts phrase
+  puts $phrase1
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts status
+  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts $status 
 end
 
 def link 
   $phrase2 = "It's Dangerous To Go Alone! Take This."
-  puts phrase
+  puts $phrase2
 end
 
 def all_phrases
   puts $phrase1
+  puts $status
   puts $phrase2
 end
